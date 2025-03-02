6595d34deac35ba78886d714bccbf6d8c5618540
EfremovM <EfremovM@lanit.ru>
2025-03-02 16:16:05 +0300

    fix emoji math

--- src/main/java/ru/mas/ktane_bot/handlers/solvers/mods/easy/EmojiMathSolver.java
+++ src/main/java/ru/mas/ktane_bot/handlers/solvers/mods/easy/EmojiMathSolver.java
@@ -3,7 +3,6 @@ package ru.mas.ktane_bot.handlers.solvers.mods.easy;
 import ru.mas.ktane_bot.cache.UserDataCache;
 import ru.mas.ktane_bot.handlers.Handler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmojiMathSolver extends Handler {
@@ -27,35 +26,12 @@ public class EmojiMathSolver extends Handler {
 
     @Override
     public String handle(String message, Long userId) {
-        var splitted_by_plus = message.split("\\+");
         userDataCache.solveModule(userId);
-        int answer = 0;
-        if (splitted_by_plus.length > 1) {
-            for (int i = 0; i < splitted_by_plus.length; i++) {
-                var result = new ArrayList<Integer>();
-                mapEmoji(splitted_by_plus, i, result);
-                answer += result.stream().reduce((a, b) -> 10 * a + b).get();
-            }
-            return String.valueOf(answer);
+        for (int i = 0; i < emoji.size() - 1; i++) {
+            message = message.replaceAll(emoji.get(i), String.valueOf(i));
         }
-        else {
-            var splitted_by_minus = message.split("-");
-            for (int i = 0; i < splitted_by_minus.length; i++) {
-                var result = new ArrayList<Integer>();
-                mapEmoji(splitted_by_minus, i, result);
-                if (i == 0)
-                    answer += result.stream().reduce((a, b) -> 10 * a + b).get();
-                else
-                    answer -= result.stream().reduce((a, b) -> 10 * a + b).get();
-            }
-            return String.valueOf(answer);
-        }
-    }
-
-    private void mapEmoji(String[] splitted, int i, List<Integer> result) {
-        var arr = splitted[i].toCharArray();
-        for (int j = 0; j < arr.length; j += 2) {
-            result.add(emoji.indexOf(arr[j] + String.valueOf(arr[j + 1])));
-        }
+        var splitted = message.split("[+\\-]");
+        return String.valueOf(message.contains("+") ? Integer.parseInt(splitted[0]) + Integer.parseInt(splitted[1]) :
+                Integer.parseInt(splitted[0]) - Integer.parseInt(splitted[1]));
     }
 }
